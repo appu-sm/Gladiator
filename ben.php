baeb345c53b4d0971511ef882e95829814edb731
aravinth <aravinth_sm@yahoo.co.in>
2018-04-20 11:20:06 +0530

    Added completed ruby and working ben code

---
+++ ben.php
@@ -0,0 +1,35 @@
+<?php
+  /* Read input from STDIN. Print your output to STDOUT*/
+  $fp = fopen('php://stdin', 'r');
+  //Write code here
+	$values = $weaponRequired = [];
+	$i = 0;
+	while($line = fgets($fp)){
+		if(empty($values)){
+			$values = explode(" ", $line);
+			$level = $values[0];
+			$weapon = $values[1];
+		}else{
+			if(count($weaponRequired) < $level){
+				$weaponRequired[$i] = $line;
+				$i++;
+				if(count($weaponRequired) == $level){
+					break;
+				}
+			}else{
+				break;
+			}
+		}
+	}
+	fclose($fp);
+	print_r($values);
+	print_r($weaponRequired);
+
+/*	
+	foreach($weaponRequired as $key => $number){
+		$required[$key] = array_sum(str_split($number));
+	}
+*/	
+	sort($weaponRequired);
+	print_r($weaponRequired);
+?>
---
+++ ruby.php
@@ -0,0 +1,89 @@
+<?php
+$fp = fopen('php://stdin', 'r');
+$necklace = [];
+while($line = fgets($fp)){
+	switch(count($necklace)){
+		case 0:
+			$necklace['blue'] = $line;
+			break;
+		case 1:
+			$necklace['red'] = $line;
+			break;
+		case 2:
+			$necklace['yellow'] = $line;
+			break;
+		case 3:
+			$necklace['green'] = $line;
+			break;
+		default:
+			break;
+	}
+	if(count($necklace) >= 4)
+		break;
+}
+fclose($fp);
+
+	if($necklace['blue'] > 0){
+		$count = $necklace['blue'];
+		$necklace['blue'] = 0;
+		red($necklace, $count, 'blue');
+	}else if($necklace['red'] > 0){
+		$count = 1;
+		$necklace['red'] -= 1;
+		green($necklace, $count);
+	}else if($necklace['green'] > 0){
+		$count = $necklace['green'];
+		$necklace['green'] = 0;
+		yellow($necklace, $count);
+	}else if($necklace['yellow'] > 0){
+		$count = 1;
+		$necklace['yellow'] -= 1;
+		last($count);
+	}else{
+		last(0);
+	}
+
+	function red($necklace, $count, $recent){
+		if($necklace['red'] > 0){
+			$count += 1;
+			$necklace['red'] -= 1;
+			green($necklace, $count);
+		}else{
+			if($recent == 'blue' || $recent == 'yellow'){
+				last($count);
+			}else{
+				green($necklace, $count);
+			}
+		}
+	}
+	
+	function green($necklace, $count){
+		if($necklace['green'] > 0){
+			$count += $necklace['green'];
+			$necklace['green'] = 0;
+			yellow($necklace, $count);
+		}else{
+			yellow($necklace, $count);
+		}
+	}
+	
+	function yellow($necklace, $count){
+		if($necklace['yellow'] > 0){
+			$count += 1;
+			$necklace['yellow'] -= 1;
+			if($necklace['red'] > 0){
+				red($necklace, $count, 'yellow');
+			}else{
+				last($count);
+			}
+		}else{
+			last($count);
+		}
+	}
+	
+	function last($count)
+	{
+		echo (int)$count;
+		exit;
+	}
+?>
