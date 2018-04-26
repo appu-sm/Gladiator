f712d74b98ffcff6cd8015993745572a0807bd60
aravinth <aravinth_sm@yahoo.co.in>
2018-04-26 08:25:35 +0530

    90/100

--- ben.php
+++ ben.php
@@ -63,27 +63,32 @@
 	function calculateCoin($array, $totalCoins){
 
 //echo "cal Arr - "; print_r($array);
-
-		if(isset($array[0]) && $array[0]['ones'] > 0){
-			$level = $array[0];
-			$coins = $level['ones'] * $level['ones'];
-			$firstLevelPositions = explode(',', $level['position']);
-			array_push($totalCoins, $coins);
-//echo "before "; print_r($array);			
-			array_shift($array);
-//echo "after "; print_r($array);			
-			$newArray = [];
-			foreach($array as $newLevel){
-				$positions = explode(',', $newLevel['position']);
-				foreach($firstLevelPositions as $firstLevelPosition){
-					if(in_array($firstLevelPosition, $positions)){
-						$newLevel['number'] = substr_replace($newLevel['number'], 0, $firstLevelPosition, 1);
+		REPEATE:
+		if(isset($array[0])){
+			if($array[0]['ones'] > 0){
+				$level = $array[0];
+				$coins = $level['ones'] * $level['ones'];
+				$firstLevelPositions = explode(',', $level['position']);
+				array_push($totalCoins, $coins);
+	//echo "before "; print_r($array);			
+				array_shift($array);
+	//echo "after "; print_r($array);			
+				$newArray = [];
+				foreach($array as $newLevel){
+					$positions = explode(',', $newLevel['position']);
+					foreach($firstLevelPositions as $firstLevelPosition){
+						if(in_array($firstLevelPosition, $positions)){
+							$newLevel['number'] = substr_replace($newLevel['number'], 0, $firstLevelPosition, 1);
+						}
 					}
+					array_push($newArray, $newLevel['number']);
 				}
-				array_push($newArray, $newLevel['number']);
+				$secArray = sortArray($newArray);
+				return calculateCoin($secArray, $totalCoins);
+			}else{
+				array_shift($array);
+				goto REPEATE;
 			}
-			$secArray = sortArray($newArray);
-			return calculateCoin($secArray, $totalCoins);
 		}else{
 			return $totalCoins;
 		}
